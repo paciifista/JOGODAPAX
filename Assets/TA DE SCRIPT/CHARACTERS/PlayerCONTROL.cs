cd87674a20b61fe6a84662b5f81c24c17b995ea6
fernando <fernandobraga0704@gmail.com>
2020-02-27 09:58:09 -0300

    THE MAN IS WALKING BRO

---
+++ Assets/TA DE SCRIPT/CHARACTERS/PlayerCONTROL.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerCONTROL : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public LayerMask WalkAble;
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetMouseButtonDown(1))
+       {
+           Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+           RaycastHit hit;
+
+           if (Physics.Raycast(ray, out hit, Mathf.Infinity, WalkAble))
+           {
+             agent.SetDestination(hit.point);  
+           }
+       }
+    }
+}
