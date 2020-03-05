b4ad12e4557be1353496a43f81eb9daea59d5a95
fernando <fernandobraga0704@gmail.com>
2020-03-05 09:03:56 -0300

    TouchAbleGameOBJ

---
+++ Assets/TA DE SCRIPT/NavGame/Core/TouchableGameOBJ.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Core
+{
+    public class TouchableGameOBJ : MonoBehaviour
+   {
+       public float contactRadius =0.5f;
+       public bool IsInTouch(TouchableGameOBJ other)
+       {
+           float distance = Vector3.Distance(transform.position, other.transform.position);
+           return distance < contactRadius + other.contactRadius;
+       }
+
+       protected virtual void OnDrawGizmosSelected()
+       {
+           Gizmos.color = Color.black;
+           Gizmos.DrawWireSphere(transform.position, contactRadius);
+       }
+    }
+}
