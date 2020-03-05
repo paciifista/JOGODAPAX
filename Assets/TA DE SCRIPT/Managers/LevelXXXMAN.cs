fcb0076d502e7a46e4a48caf0f1692551d87690e
fernando <fernandobraga0704@gmail.com>
2020-03-05 08:39:09 -0300

    Creepy Spawn

--- Assets/TA DE SCRIPT/Managers/LevelXXXMAN.cs
+++ Assets/TA DE SCRIPT/Managers/LevelXXXMAN.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class LevelXXXMAN : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] badSpawn;
+    public GameObject badPrefab;
+    public int badWaves = 3;
+    public float waitTimeFirstWave = 2f;
+    public float waiTimeBetweenWaves = 4f;
+
     void Start()
     {
-        
+        StartCoroutine(SpawnBad());
     }
 
     // Update is called once per frame
-    void Update()
+    IEnumerator SpawnBad()
     {
-        
+        yield return new WaitForSeconds(waitTimeFirstWave);
+        for (int i = 0; i < badWaves; i++ )
+        {
+            for (int j = 0; j <badSpawn.Length; j++)
+            {
+                
+                Instantiate(badPrefab, badSpawn[j].position, Quaternion.identity);
+
+
+            }
+            yield return new WaitForSeconds (waiTimeBetweenWaves);
+        }
     }
 }
