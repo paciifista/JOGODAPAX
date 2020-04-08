26698639d30fd8fd2384acb88551ea247102a983
Fernando Braga <60928801+paciifista@users.noreply.github.com>
2020-04-08 12:25:35 -0300

    RedParticles preFAP

---
+++ Assets/TA DE SCRIPT/NavGame/misskiss/DestroyWithDelay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.misskiss
+
+{
+    public class DestroyWithDelay : MonoBehaviour
+    {
+        public float delay = 2;
+        void Start()
+        {
+            Destroy(gameObject, delay);
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
