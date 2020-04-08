90e8a971bfeca0742e2064adf0fa950642e43c24
Fernando Braga <60928801+paciifista@users.noreply.github.com>
2020-04-08 15:00:15 -0300

    SFXController E....

---
+++ Assets/TA DE SCRIPT/NavGame/SFX/DamageAbleEffects.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NavGame.Core;
+
+
+namespace NavGame.Effects
+{
+    [RequireComponent (typeof(DamageAbleGameOBJ))]
+    public class DamageAbleEffects : SFXController
+    {
+        public string damageSound;
+        public string dieSound;
+        public GameObject damageEffects;
+        public GameObject dieEffect;
+        void Awake()
+        {
+            DamageAbleGameOBJ damageAble = GetComponent<DamageAbleGameOBJ>();
+            damageAble.onDamageTaken += OnDamageTaken;
+            damageAble.onDied += OnDied;
+        }
+
+        protected virtual void OnDamageTaken(Vector3 strikePoint, int amount)
+        {
+            PlayEffects (strikePoint, damageSound, damageEffects, Quaternion.identity);
+        }
+
+        protected virtual void OnDied()
+        {
+            PlayEffects (transform.position, dieSound, dieEffect, Quaternion.identity);
+        }
+
+
+    }
+
+}
