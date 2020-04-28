e0c1046c5fd95b7f54f5a84f8a8137c5314621e8
Fernando Braga <60928801+paciifista@users.noreply.github.com>
2020-04-28 11:52:10 -0300

    fidget spinner

---
+++ Assets/TA DE SCRIPT/NavGame/misskiss/Spinner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+namespace NavGame.misskiss
+{
+    public class Spinner : MonoBehaviour
+    {
+      public Vector3 eulersPerSecond;
+      void Update()
+      {
+          transform.Rotate(eulersPerSecond*Time.deltaTime);
+      }
+    }
+}
