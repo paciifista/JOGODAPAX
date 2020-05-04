6bc50e161ecfdaa4ee73ec0e32e89f9f27928104
Fernando Braga <60928801+paciifista@users.noreply.github.com>
2020-05-04 13:43:52 -0300

    UIManager

---
+++ Assets/TA DE SCRIPT/Managers/UIMAN.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMAN : MonoBehaviour
+{
+    public GameObject[] cooldownOBJs;
+    Image[] cooldownImages; 
+    void Start()
+    {
+        InitializeUI();
+    }
+    void InitializeUI()
+    {
+        cooldownImages = new Image [cooldownOBJs.Length];
+        for (int i = 0; i < cooldownOBJs.Length; i++)
+        {
+            cooldownImages[i] = cooldownOBJs[i].GetComponent<Image>();
+            cooldownImages[i].fillAmount = 0f;
+        }
+
+    }
+}
