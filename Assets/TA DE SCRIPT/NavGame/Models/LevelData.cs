8f926593b85bd8d61c88dd8c5cebe1ab01f34ef3
Fernando Braga <60928801+paciifista@users.noreply.github.com>
2020-05-08 19:40:54 -0300

    Lvel Data

---
+++ Assets/TA DE SCRIPT/NavGame/Models/LevelData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace NavGame.Models
+{
+
+    public class LevelData
+    {
+        public int CointCount { get; private set; }
+        public void AddCoins(int amount)
+        {
+            CointCount += amount;
+        }
+
+
+        public void ConsumeCoins(int amount)
+        {
+            ValidadeCoinAmount(amount);
+            CointCount -= amount;
+        }
+
+        public void ValidadeCoinAmount(int amount)
+        {
+            if (CointCount < amount)
+            {
+                throw new InvalidOperationException("Need " + amount + " coins");
+            }
+        }
+
+
+    }
+
+
+}
